bd1b70fd5c5ab3688e6d9dea1ea18c690b16303c
BHAVIT-SINGLA <bhavithisar2001@gmail.com>
2021-02-04 21:57:24 +0530

    STOP FUNCTIONALITY

--- app.js
+++ app.js
@@ -10,11 +10,14 @@ console.log(newspeed);
 value1.style.animationDuration=`${newspeed}`+'s';
 value2.style.animationDuration=`${newspeed}`+'s';
 value3.style.animationDuration=`${newspeed}`+'s';
+let stopb=document.getElementById('stop');
+console.log(stopb);
 
-
+var animationId;
 function getValues()   
 {
-    setInterval(() => {
+  
+    animationId=setInterval(() => {
         let req=[];
         for(let i=0;i<3;i++)
         {
@@ -28,21 +31,71 @@ function getValues()
         
     }, 900);
    
-   
 };
+getValues();
+
+
+function updateSpeed (newspeed,tostop)
+{
+    if(tostop)
+    {
+        clearInterval
+    }
+    if(animationId)
+    {
+        clearInterval(animationId);
+    }
+    animationId=   setInterval(() => {
+        let req=[];
+        for(let i=0;i<3;i++)
+        {
+        let val1= values[Math.floor(Math.random()*8)];
+        req.push(val1);
+        
+        }
+        value1.innerText=req[0];
+        value2.innerText=req[1];
+        value3.innerText=req[2];
+        
+    }, 1000/`${newspeed}`);
+
+}
 
 speed.onchange =(ev) =>
 {
   
    
     var u=ev.target.value;
-     newspeed=1/u;
+     newspeed=3/u;
     value1.style.animationDuration=`${newspeed}`+'s';
     value2.style.animationDuration=`${newspeed}`+'s';
     value3.style.animationDuration=`${newspeed}`+'s';
+    updateSpeed(newspeed);
+}
+stopb.onclick =() =>
+{
+    console.log(stopb);
+    value1.style.animationDuration="";
+    value2.style.animationDuration="";
+    value3.style.animationDuration="";
+    clearInterval(animationId);
+    let v1=value1.innerText;
+    let v2=value2.innerText;
+    let v3=value3.innerText;
+    if(v1===v2 && v1===v3)
+    {
+        console.log("YOU WON THE GAME");
+    }
+    else
+    {
+        console.log("TRY AGAIN");
+    }
     
+
 }
 
 
-getValues();
+
+
+
 
